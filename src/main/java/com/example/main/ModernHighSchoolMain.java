26188fb5cca82d5b8cf16e6d79d03d135775e090
kamaljanghel <kamaljanghel7@gmail.com>
2022-10-19 09:58:06 +0530

    added source files
    added source files

---
+++ src/main/java/com/example/main/ModernHighSchoolMain.java
@@ -0,0 +1,15 @@
+package com.example.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernHighSchoolMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModernHighSchoolMain.class, args);
+		
+		System.out.println("Hello");
+	}
+
+}
